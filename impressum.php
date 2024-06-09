77cd8ca5240e23d0b73d7823f028faa9a2f0032e
franziskamichl <Franziska.Michl@stud.th-deg.de>
2024-06-09 13:43:14 +0200

    Textänderung Impressum

--- impressum.php
+++ impressum.php
@@ -24,7 +24,7 @@
         
         <h4>Angaben gemäß § 5 TMG:</h4>
         <p>Franz Stumbeck</p>
-        <p>Adresse: Firmenadresse/Privatadresse</p>
+        <p>Adresse: </p>
         <br>
         <p></a></p>
         <br>
@@ -38,9 +38,10 @@
         <p>E-Mail: franzstumbeck@gmail.com</p>
         <br>
         <h5>Haftung für Inhalte</h5>
-        <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+        <p>Veranwortlich für den Inhalt nach § 55 Abs. 2 RStV:</p><br>
+        <p>Franz Stumbeck, Adresse</p>
         <br>
-        <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+        <p></p>
         <br>
         <h5>Haftung für Links</h5>
         <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
@@ -51,6 +52,10 @@
         <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
         <br>
         <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+        <h5>Verwendung von Sozialen Medien:</h5>
+        <p> Dieses Impressum gilt auch für Auftritte in den sozial Medien, außerdem werden dadurch deren Nutzungsbedigungen geltend gemacht.</p>
+        <p>Facebook: https://de-de.facebook.com/legal/terms#:~:text=Du%20darfst%20Benutzernamen%20oder%20Passw%C3%B6rter,in%20den%20Plattform%2DNutzungsbedingungen%20festgelegt.</p>
+        <p>Instagram: https://help.instagram.com/581066165581870</p>
     </section>
 
 
