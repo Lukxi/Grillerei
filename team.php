1f8596d2c2570fe17f103bb8f54bdca72f4e28d3
Lukxi <nowak.lukas03@outlook.de>
2024-06-10 12:52:40 +0200

    replaced footer implementation to php include

--- team.php
+++ team.php
@@ -50,45 +50,8 @@
     </section>
 
 
-    <section class="footer">
-        <div class="f-row">
-            <div class="f-col">
-                <img src="img/logo.png" alt="logo" class="f-logo">
-                <div class="f-p">
-                    <p>Als Verein des Studiengangs Wirtschaftsinformatik an der THD haben wir es uns zur Aufgabe gemacht WI-Studenten zu helfen, sich schnell im Studiengang, an der Hochschule und natürlich im Studentenleben zurecht zu finden.</p>
-                </div>
-            </div>
-            <div class="f-col">
-                <h3>Adresse <div class="underline"><span></span></div></h3>
-                <p>Raum A209</p>
-                <p>Dieter-Görlitz-Platz 1</p>
-                <p>94469 Deggendorf</p>
-                <h4 class="mail"><a href="mailto:wiproject@th-deg.de">wiproject@th-deg.de</a></h4>
-            </div>
-            <div class="f-col">
-                <h3>Links <div class="underline"><span></span></div></h3>
-                    <ul>                        
-                        <li><a href="impressum.html" id="impressum">Impressum</a></li>
-                        <li><a href="impressum.html#daten" id="impressum">Datenschutz</a></li>
+    <?php include('includes/footer.php'); ?>
 
-                    </ul>
-                
-            </div>
-            <div class="f-col">
-                <h3>Social <div class="underline"><span></span></div></h3>
-                <div class="social-icons">
-                    <a href="https://www.instagram.com/wiproject_thd/"><i class="fab fa-instagram"></i></a>
-                    <a href="https://www.facebook.com/WIProjecteV/"><i class="fab fa-facebook"></i></a>
-                </div>
-            </div>
-            
-
-        </div>
-        <hr>
-        <p class="copy">&copy; 2024 WI-Project e.V. - Made with <i class="fa-regular fa-heart"></i> in Deggendorf</p>
-
-        
-    </section>
 
     <script>
 
