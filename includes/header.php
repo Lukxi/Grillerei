d0371d7b54422f939a8bf331c30f391df285674d
Lukxi <nowak.lukas03@outlook.de>
2024-06-20 08:50:47 +0200

    fixed nav bug

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
             <div class="nav-links" id="navLinks">
                 <ul>
                     <li><a href="index.php">Startseite</a></li>
-                    <li><a href="index.php#about">Über uns</a><li>
+                    <li><a href="index.php#about">Meine Angebote</a></li>
                     <li><a href="appetitmacher.php">Appetitmacher</a></li>
                     <li><a href ="team.php">Team</a></li>
                 </ul>
@@ -18,7 +18,7 @@
         </nav>
         <div class="dropdown">
             <li><a href="index.php">Startseite</a></li>
-            <li><a href="index.php#about">Über uns</a></li>
+            <li><a href="index.php#about">Meine Angebote</a></li>
             <li><a href="appetitmacher.php">Appetitmacher</a></li>
             <li><a href ="team.php">Team</a></li>
             <li><a id="n-btn" href="index.php#kontakt">Kontakt</a></li>
