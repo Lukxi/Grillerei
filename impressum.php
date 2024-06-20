9ab4f24bc831ebe78236d9672d6b1a5f9d30ef0b
franziskamichl <Franziska.Michl@stud.th-deg.de>
2024-06-20 09:08:28 +0200

    Bildnachweis

--- impressum.php
+++ impressum.php
@@ -53,10 +53,12 @@
         <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
         <br>
         <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+        <br>
         <h5>Verwendung von Sozialen Medien:</h5>
         <p> Dieses Impressum gilt auch für Auftritte in den sozial Medien, außerdem werden dadurch deren Nutzungsbedigungen geltend gemacht.</p>
         <p>Facebook: <a href="https://de-de.facebook.com/legal/terms#:~:text=Du%20darfst%20Benutzernamen%20oder%20Passw%C3%B6rter,in%20den%20Plattform%2DNutzungsbedingungen%20festgelegt">Facebook-Nutzungsbedingungen</a></p>
         <p>Instagram: <a href="https://help.instagram.com/581066165581870">Instagram-Nutzungsbedinungen</a></p>
+        <br>
         <h5>Bildnachweise:</h5>
         <p>Die auf diese Webseite verwendeten Bilder stammen entweder von mir selbst oder sind lizenzfrei und wurden von folgenden Plattformen bezogen:</p>
         <p>Pixabay</p>
